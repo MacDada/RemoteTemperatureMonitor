6a0b6d80944f1f46e562d278b6559f939bd1900f
Dawid Nowak <code@dnowak.pl>
2016-11-21 00:58:02 +0100

    wifi.lua refactored: functions defined inside the table

--- src/nodemcu-lua/wifi.lua
+++ src/nodemcu-lua/wifi.lua
@@ -1,42 +1,42 @@
-d_wifi = {}
+d_wifi = {
+    printConnectionDetails = function()
+        if not d_wifi.isConnected() then
+            print("No wifi connection!")
+            return
+        end
 
-function d_wifi.printConnectionDetails()
-    if not d_wifi.isConnected() then
-        print("No wifi connection!")
-        return
+        local ssid = wifi.sta.getconfig()
+        print('Connected to: "'..ssid..'"')
+
+        local ip, netmask, gateway = wifi.sta.getip()
+        print('ip: "'..ip..'"')
+        print('netmask: "'..netmask..'"')
+        print('gateway: "'..gateway..'"')
+    end,
+
+    isConnected = function()
+        return nil ~= wifi.sta.getip()
+    end,
+
+    connect = function(ssid, pass, onReady)
+        print('Connecting to "'..ssid..'"')
+
+        wifi.setmode(wifi.STATION)
+        wifi.sta.config(ssid, pass)
+        --    wifi.sta.setip({
+        --        ip = "192.168.0.26",
+        --        netmask = "255.255.255.0",
+        --        gateway = "192.168.0.1"
+        --    })
+        wifi.sta.connect()
+
+        tmr.alarm(1, 1000, 1, function()
+            if not d_wifi.isConnected() then
+                print("Wifi not ready, waiting...")
+            else
+                tmr.stop(1)
+                onReady()
+            end
+        end)
     end
-
-    local ssid = wifi.sta.getconfig()
-    print('Connected to: "'..ssid..'"')
-     
-    local ip, netmask, gateway = wifi.sta.getip()
-    print('ip: "'..ip..'"')
-    print('netmask: "'..netmask..'"')
-    print('gateway: "'..gateway..'"')
-end
-
-function d_wifi.isConnected()
-    return nil ~= wifi.sta.getip()
-end
-
-function d_wifi.connect(ssid, pass, onReady)
-    print('Connecting to "'..ssid..'"')
-    
-    wifi.setmode(wifi.STATION)
-    wifi.sta.config(ssid, pass)
---    wifi.sta.setip({
---        ip = "192.168.0.26",
---        netmask = "255.255.255.0",
---        gateway = "192.168.0.1"
---    })
-    wifi.sta.connect()
-    
-    tmr.alarm(1, 1000, 1, function() 
-        if not d_wifi.isConnected() then 
-            print("Wifi not ready, waiting...") 
-        else 
-            tmr.stop(1)
-            onReady()
-        end 
-    end)
-end
+}
