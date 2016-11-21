85ee95711462e325f6470454b17e5b85a2226649
Dawid Nowak <code@dnowak.pl>
2016-11-21 02:53:22 +0100

    extracted constant

--- src/nodemcu-lua/init2.lua
+++ src/nodemcu-lua/init2.lua
@@ -7,14 +7,16 @@ dofile('thermometer.lua')
 local WIFI_NAME = d_config.wifis[d_config.wifi]
 local WIFI_PASSWORD = d_credentials[d_config.wifi]
 
+local THINGSPEAK_CHANNEL_FIELD = 2
+
 d_wifi.connect(WIFI_NAME, WIFI_PASSWORD, function()
     d_wifi.printConnectionDetails()
 
     local thingspeakApi = d_thingspeak.new(d_credentials.thingspeak_api_key)
 
-    thingspeakApi:update(2, d_thermometer.measureTemperature())
+    thingspeakApi:update(THINGSPEAK_CHANNEL_FIELD, d_thermometer.measureTemperature())
 
     tmr.alarm(0, d_config.timeout_between_measures_in_seconds*1000, 1, function()
-        thingspeakApi:update(2, d_thermometer.measureTemperature())
+        thingspeakApi:update(THINGSPEAK_CHANNEL_FIELD, d_thermometer.measureTemperature())
     end)
 end)
