ed048d08e20fcd61a9684fa7330da0d8a7674036
Dawid Nowak <code@dnowak.pl>
2016-11-22 23:12:44 +0100

    thermometer: getting addresses moved from setup to measuring part, as multiple thermometers could be monitored by plug and play (without the need for restart)

--- src/nodemcu-lua/thermometer.lua
+++ src/nodemcu-lua/thermometer.lua
@@ -4,17 +4,11 @@ local SENSOR_ERROR_TEMPERATURE = 85;
 
 local dsApi = require("ds18b20")
 
-local addrs
-
 d_thermometer = {
     initialized = false,
 
     initialize = function()
         dsApi.setup(d_config.thermometer_input_pin)
-
-        addrs = dsApi.addrs()
-
-        print("Total DS18B20 sensors: "..table.getn(addrs))
 
         d_thermometer.initialized = true
     end,
@@ -23,6 +17,10 @@ d_thermometer = {
         if (false == d_thermometer.initialized) then
             d_thermometer.initialize()
         end
+
+        local addrs = dsApi.addrs()
+
+        print("Total DS18B20 sensors: "..table.getn(addrs))
 
         local temperature = dsApi.read(addrs[1], dsApi.C)
         print("Measured temp: ".. temperature)
